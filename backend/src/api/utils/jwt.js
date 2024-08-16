279bc6411e4fd58fe286db6b08c36760f09d96e2
Müslim Yılmaz <mslmyilmaz5@gmail.com>
2024-08-16 18:06:39 +0300

    Update jwt.js

--- backend/src/api/utils/jwt.js
+++ backend/src/api/utils/jwt.js
@@ -16,8 +16,8 @@ const attachCookiesToResponse = ({res, user}) => {
     res.cookie('token',token,{
         httpOnly: true,
         expires: new Date(Date.now() + oneDay),
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None', // Allow the cookie to be sent in cross-site requests
+        secure: true,
+        sameSite: "none", // Allow the cookie to be sent in cross-site requests
         //signed: true,
        
     })
