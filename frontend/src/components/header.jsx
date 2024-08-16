4b88c5fc645ee9f9052140891e1e545abe0f7652
Müslim Yılmaz <mslmyilmaz5@gmail.com>
2024-08-16 20:11:27 +0300

    Update header.jsx

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,41 +1,39 @@
-
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogOut } from '../hooks/useLogOut';
+import { Link, useNavigate } from 'react-router-dom';
 import '../css/header.css';
-import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const { logout } = useLogOut()
-  const { user } = useAuthContext()
+  const { logout } = useLogOut();
+  const { user } = useAuthContext();
   const navigate = useNavigate();
 
   const handleClick = () => {
-    logout()
-  }
+    logout();
+  };
 
   return (
     <header>
       <div id="header-content">
         <div id="main">
-        <a href="/">MÜTEFERRİÇ</a>
+          <Link to="/">MÜTEFERRİÇ</Link>
         </div>
         <div id="poem-page-link">
-        { user ? <a href="/myPoetries">Profilim</a> : ''}
+          {user && <Link to="/myPoetries">Profilim</Link>}
         </div>
       </div>
-      {user && (
+      {user ? (
         <div className="header-buttons">
           <button className="header-button" onClick={handleClick}>Çıkış yap</button>
         </div>
-      )}
-      {!user && (
+      ) : (
         <div className="header-buttons">
           <button className="header-button" onClick={() => navigate('/login')}>Giriş yap</button>
           <button className="header-button" onClick={() => navigate('/signup')}>Kayıt Ol</button>
         </div>
       )}
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
