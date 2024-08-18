6a63233b7a817daa3a27729628f51bae5e4a8b57
Müslim Yılmaz <mslmyilmaz5@gmail.com>
2024-08-18 18:34:08 +0300

    db connection optimization

--- backend/src/api/db/connection.js
+++ backend/src/api/db/connection.js
@@ -1,13 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = (url) => {
     try {
-        mongoose.connect(url);
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, 
+            socketTimeoutMS: 45000,  
+            poolSize: 10            
+        });
         console.log('DATABASE CONNECTION IS OK!');
     } catch (error) {
         console.error('DATABASE CONNECTION ERROR:', error);
-        throw error; 
+        throw error;
+    }
+};
+
+// Bağlantıyı sürekli aktif tutmak için ping fonksiyonu
+function keepMongoAlive() {
+    if (mongoose.connection.readyState === 1) {
+        mongoose.connection.db.admin().ping((err, result) => {
+            if (err) {
+                console.error('Ping error:', err);
+            } else {
+                console.log('Ping result:', result);
+            }
+        });
     }
 }
+
+// Her 4 dakikada bir ping göndererek bağlantıyı aktif tut
+setInterval(keepMongoAlive, 4 * 60 * 1000);
 
 module.exports = connectDB;
